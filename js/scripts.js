ae605b766f2ed68484d6efaf2c4913fa7816f5bd
raeman21 <mandelar94@gmail.com>
2018-12-14 16:35:26 +0300

    padding increase, add background color and colors to classes

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,21 @@
 
-function Dice() {
+function dice_Roll() {
   this.die = [];
   this.roll = function(roll) {
     this.die = [Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1]
     return this; // for chaining
   }
   this.printDie = function() {
-    document.getElementById('die1').innerHTML = this.die[0];
-    document.getElementById('die2').innerHTML = this.die[1];
+    document.getElementById('#die1').innerHTML = this.die[0];
+    document.getElementById('#die2').innerHTML = this.die[1];
   }
   return this;
 }
 
- window.onload=function(() {
-  document.getElementById("rollBut").onclick=function() {
+ window.onload=function() {
+  document.getElementById("#rollBut").onclick=function() {
     var die = new Dice()
     die.roll().printDie();
   }
-}
-function play1(){
-  $(document).ready(function()
-)
-}
+
 }
