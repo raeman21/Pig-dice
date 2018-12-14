576bdb2d16250346513e8dbc2d927944e4e0c03c
raeman21 <mandelar94@gmail.com>
2018-12-14 11:47:14 +0300

    add google fonts, instructions and change html layout, add bootstrap class(well)

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+
+function Dice() {
+  this.die = [];
+  this.roll = function(roll) {
+    this.die = [Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1]
+    return this; // for chaining
+  }
+  this.printDie = function() {
+    document.getElementById('die1').innerHTML = this.die[0];
+    document.getElementById('die2').innerHTML = this.die[1];
+  }
+  return this;
+}
+
+ window.onload=function(() {
+  document.getElementById("rollBut").onclick=function() {
+    var die = new Dice()
+    die.roll().printDie();
+  }
+}
+function play1(){
+  $(document).ready(function()
+)
+}
+}
